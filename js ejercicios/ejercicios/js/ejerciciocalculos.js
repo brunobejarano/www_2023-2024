df5416a7b2186ea012c97e6e59638f85cfc26020
brunobejarano <152491211+brunobejarano@users.noreply.github.com>
2024-03-21 11:38:04 +0100

    Add files via upload

---
+++ js ejercicios/ejercicios/js/ejerciciocalculos.js
@@ -0,0 +1,56 @@
+//suma números//////////////////
+
+let num1 = 4;    
+let num2 = 6;
+let result = num1 + num2;
+
+
+console.log("LA PRIMERA SUMA VALE" + result);
+
+// suma de número y texto//
+
+
+let x = 5;
+let Y = "agustilin";
+let result2 = x + Y;
+
+console.log("la segunda suma vale" + result2);  
+
+//resta productos//
+
+let num3 = 4;    
+let num4 = 6;
+let result3 = num1 - num2;
+
+console.log("LA PRIMERA resta VALE" + result3);
+
+
+// postincremento//
+
+num1++;
+result = num1
+
+console.log("Despues del incremento de num,el resultado vale" + result) 
+
+num1--; 
+result = num1
+
+console.log("Despues del incremento de num,el resultado vale" + result) 
+
+//document write
+
+let num8 = 18;
+let num9 = 3;
+
+
+let resultados = num8 + num9;
+
+
+
+
+//document.write("la suma de los valores da "+ resultados)
+
+
+
+
+document.getElementById("resultados").innerHTML = "la primera se desespera" + resultados
